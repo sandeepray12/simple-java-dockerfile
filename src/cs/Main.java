5129359cc113816af9881a30badd13a93ae94737
shrikant sharma <shrikant.sharma@nureca.com>
2025-07-09 23:28:07 +0530

    Implement simple java code with docker file

---
+++ src/cs/Main.java
@@ -0,0 +1,10 @@
+package cs;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome to !");
+        System.out.println("Coder Sandy channel");
+    }
+}
